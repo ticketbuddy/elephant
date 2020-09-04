1b88be346cd48e084c85771d1035fdbf174d3b3a
James Bowers <james.bowers@bbc.co.uk>
2020-09-04 16:04:31 +0100

    postgres store testable

--- lib/elephant/store/postgres.ex
+++ lib/elephant/store/postgres.ex
@@ -1,46 +1,59 @@
 defmodule Elephant.Store.Postgres do
+  alias Elephant.Memory
+  alias Elephant.Store.Postgres.Schema
+
+  def put(repo, memory = %Memory{}) do
+    repo.insert(%Schema.Memory{
+      target: memory.at,
+      action: :erlang.term_to_binary(memory.action)
+    })
+    |> case do
+      {:ok, _memory} -> :ok
+      {:error, reason} -> :error
+    end
+  end
+
+  def fetch(repo, up_to_datetime, callback) do
+    import Ecto.Query, only: [from: 2]
+
+    query = from(m in Schema.Memory, where: m.target <= ^up_to_datetime, order_by: m.target)
+
+    stream = repo.all(query)
+
+    repo.transaction(fn ->
+      stream
+      |> Stream.map(fn memory ->
+        {memory.memory_id, memory.target, :erlang.binary_to_term(memory.action)}
+      end)
+      |> callback.()
+    end)
+    |> case do
+      {:ok, true} -> :ok
+      _error -> :error
+    end
+  end
+
+  def delete(repo, memory_id) do
+    repo.delete(%Schema.Memory{memory_id: memory_id}, stale_error_field: :errors)
+
+    :ok
+  end
+
   defmacro __using__(repo: repo) do
     quote do
       @behaviour Elephant.Store
       @repo unquote(repo)
-      alias Elephant.Memory
-      alias Elephant.Store.Postgres.Schema
 
       def put(memory = %Memory{}) do
-        @repo.insert(%Schema.Memory{
-          target: memory.at,
-          action: :erlang.term_to_binary(memory.action)
-        })
-        |> case do
-          {:ok, _memory} -> :ok
-          {:error, reason} -> :error
-        end
+        Elephant.Store.Postgres.put(@repo, memory)
       end
 
       def fetch(up_to_datetime, callback) do
-        import Ecto.Query, only: [from: 2]
-
-        query = from(m in Schema.Memory, where: m.target <= ^up_to_datetime, order_by: m.target)
-
-        stream = @repo.all(query)
-
-        @repo.transaction(fn ->
-          stream
-          |> Stream.map(fn memory ->
-            {memory.memory_id, memory.target, :erlang.binary_to_term(memory.action)}
-          end)
-          |> callback.()
-        end)
-        |> case do
-          {:ok, true} -> :ok
-          _error -> :error
-        end
+        Elephant.Store.Postgres.fetch(@repo, up_to_datetime, callback)
       end
 
       def delete(memory_id) do
-        @repo.delete(%Schema.Memory{memory_id: memory_id}, stale_error_field: :errors)
-
-        :ok
+        Elephant.Store.Postgres.delete(@repo, memory_id)
       end
     end
   end
--- test/elephant/focus_test.exs
+++ test/elephant/focus_test.exs
@@ -26,16 +26,17 @@ defmodule Elephant.FocusTest do
 
   test "fetches and runs callbacks" do
     test_pid = self()
+    now = DateTime.utc_now()
 
     callbacks = [
       {
         "memory-id-1",
-        Clock.time_travel(DateTime.utc_now(), {1, :seconds}),
+        Clock.time_travel(now, {1, :seconds}),
         {FakeCallback, :run, [test_pid, :done_first]}
       },
       {
         "memory-id-2",
-        Clock.time_travel(DateTime.utc_now(), {2, :seconds}),
+        Clock.time_travel(now, {2, :seconds}),
         {FakeCallback, :run, [test_pid, :done_second]}
       }
     ]
@@ -58,7 +59,7 @@ defmodule Elephant.FocusTest do
     Elephant.Focus.handle_info(:next, :no_state)
 
     # no message for first 900 milliseconds
-    refute_receive(:done_first, 990)
+    refute_receive(:done_first, 900)
 
     # then should receive within next x milliseconds
     assert_receive(:done_first, 100)
